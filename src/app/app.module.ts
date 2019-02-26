2fb596c8fe135601af4698e410095265d76d1ef7
Zoey Child <imperialoctopus@gmail.com>
2019-02-26 16:33:57 +0000

    Install dexie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,24 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppComponent } from "./app.component";
-import { MenuComponent } from "./menu/menu.component";
-import { PlayComponent } from "./play/play.component";
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { PlayComponent } from './play/play.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
 const appRoutes: Routes = [
-  { path: "", component: MenuComponent },
-  { path: "play/:n", component: PlayComponent },
-  { path: "**", component: MenuComponent }
+  { path: '', component: MenuComponent },
+  { path: 'play/:n', component: PlayComponent },
+  { path: '**', component: MenuComponent }
 ];
 
 @NgModule({
   declarations: [AppComponent, MenuComponent, PlayComponent],
-  imports: [BrowserModule, RouterModule.forRoot(appRoutes), ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
