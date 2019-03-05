881c8595c31c42f98aa515ee64bed334386f7717
Zoey Child <imperialoctopus@gmail.com>
2019-03-05 18:28:03 +0000

    Add material list element

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
-import { PlayComponent } from './play/play.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from "./app.component";
+import { MenuComponent } from "./menu/menu.component";
+import { PlayComponent } from "./play/play.component";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { environment } from "../environments/environment";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { MatListModule } from "@angular/material/list";
 
 const appRoutes: Routes = [
-  { path: '', component: MenuComponent },
-  { path: 'play/:n', component: PlayComponent },
-  { path: '**', component: MenuComponent }
+  { path: "", component: MenuComponent },
+  { path: "play/:n", component: PlayComponent },
+  { path: "**", component: MenuComponent }
 ];
 
 @NgModule({
   declarations: [AppComponent, MenuComponent, PlayComponent],
-  imports: [BrowserModule, RouterModule.forRoot(appRoutes),
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    ServiceWorkerModule.register("ngsw-worker.js", {
+      enabled: environment.production
+    }),
+    BrowserAnimationsModule,
+    MatListModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { MatListModule } from "@angular/material/list";
 
 @Component({
   selector: "app-menu",
