dc285307d350a2001e028f3021b3b48477fa6036
Zoey Child <imperialoctopus@gmail.com>
2019-03-05 22:38:20 +0000

    Add click toggling for oberon and morganna

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,33 +1,33 @@
-import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
-import { MatListModule } from "@angular/material/list";
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
-  selector: "app-menu",
-  templateUrl: "./menu.component.html",
-  styleUrls: ["./menu.component.scss"]
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
   playerNumber: number;
+  oberonEnabled: boolean;
+  morgannaEnabled: boolean;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) { }
 
   ngOnInit() {
-    this.playerNumber = 3;
+    this.playerNumber = 5;
+    this.oberonEnabled = false;
+    this.morgannaEnabled = false;
   }
 
   play() {
-    this.router.navigate(["/play", this.playerNumber]);
+    this.router.navigate(['/play', this.playerNumber]);
   }
 
-  numberUp() {
-    if (this.playerNumber < 32) {
-      this.playerNumber += 1;
-    }
+  oberon() {
+    this.oberonEnabled = !this.oberonEnabled;
   }
-  numberDown() {
-    if (this.playerNumber > 3) {
-      this.playerNumber -= 1;
-    }
+  morganna() {
+    this.morgannaEnabled = !this.morgannaEnabled;
   }
 }
