8c6bf55b8726118314cdd8c3956fc8474f7e3036
Zoey Child <imperialoctopus@gmail.com>
2019-03-06 23:22:37 +0000

    Add material form to settings dialog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,52 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
-import { PlayComponent } from './play/play.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from "./app.component";
+import { MenuComponent } from "./menu/menu.component";
+import { PlayComponent } from "./play/play.component";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { environment } from "../environments/environment";
 
-import { MatListModule } from '@angular/material/list';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material';
-import { PlayerDialogComponent } from './player-dialog/player-dialog.component';
-import { SettingsDialogComponent } from './settings-dialog/settings-dialog.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatListModule } from "@angular/material/list";
+import { MatButtonModule } from "@angular/material/button";
+import { MatDialogModule } from "@angular/material";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+
+import { PlayerDialogComponent } from "./player-dialog/player-dialog.component";
+import { SettingsDialogComponent } from "./settings-dialog/settings-dialog.component";
 
 const appRoutes: Routes = [
-  { path: '', component: MenuComponent },
-  { path: 'play/:n', component: PlayComponent },
-  { path: '**', component: MenuComponent }
+  { path: "", component: MenuComponent },
+  { path: "play/:n", component: PlayComponent },
+  { path: "**", component: MenuComponent }
 ];
 
 @NgModule({
-  declarations: [AppComponent, MenuComponent, PlayComponent, PlayerDialogComponent, SettingsDialogComponent],
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    PlayComponent,
+    PlayerDialogComponent,
+    SettingsDialogComponent
+  ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes),
-    ServiceWorkerModule.register('ngsw-worker.js', {
+    ServiceWorkerModule.register("ngsw-worker.js", {
       enabled: environment.production
     }),
     BrowserAnimationsModule,
-    MatListModule, MatButtonModule, MatDialogModule
+    MatListModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent],
   entryComponents: [PlayerDialogComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/settings-dialog/settings-dialog.component.ts
+++ src/app/settings-dialog/settings-dialog.component.ts
@@ -1,36 +1,21 @@
 import { Component, OnInit, Inject } from "@angular/core";
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material";
 import { SettingsService } from "../settings.service";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
 
 @Component({
-  selector: "app-player-dialog",
-  templateUrl: "./player-dialog.component.html",
-  styleUrls: ["./player-dialog.component.scss"]
+  selector: "app-settings-dialog",
+  templateUrl: "./settings-dialog.component.html",
+  styleUrls: ["./settings-dialog.component.scss"]
 })
-export class PlayerDialogComponent implements OnInit {
-  playerNum: number;
-
+export class SettingsDialogComponent implements OnInit {
   constructor(
-    private dialogRef: MatDialogRef<PlayerDialogComponent>,
+    private dialogRef: MatDialogRef<SettingsDialogComponent>,
     private settingsService: SettingsService
   ) {}
 
-  ngOnInit() {
-    this.playerNum = this.settingsService.playerNumber;
-  }
-
-  down() {
-    if (this.playerNum > 5) {
-      this.playerNum -= 1;
-    }
-    this.settingsService.setPlayerNumber(this.playerNum);
-  }
-  up() {
-    if (this.playerNum < 10) {
-      this.playerNum += 1;
-    }
-    this.settingsService.setPlayerNumber(this.playerNum);
-  }
+  ngOnInit() {}
 
   close() {
     this.dialogRef.close();
