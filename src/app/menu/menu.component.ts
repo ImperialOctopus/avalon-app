fef4f799b3646089d78fd884e61407a49ef89134
Zoey Child <imperialoctopus@gmail.com>
2019-03-06 11:25:03 +0000

    Add assassin counting

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
-import { MatListModule } from "@angular/material/list";
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
-  selector: "app-menu",
-  templateUrl: "./menu.component.html",
-  styleUrls: ["./menu.component.scss"]
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
-  evil: Object = {
+  evil: object = {
     5: 2,
     6: 2,
     7: 3,
@@ -16,49 +16,48 @@ export class MenuComponent implements OnInit {
     9: 3,
     10: 4
   };
-  characters: Object = {
+  characters: object = {
     percival: false,
     mordred: false,
     oberon: false,
     morgana: false
   };
 
-  playerNumber: number = 5;
+  playerNumber = 5;
   servantNumber: number;
   minionNumber: number;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) { }
 
   ngOnInit() {
     this.updateNumbers();
   }
 
   play() {
-    this.router.navigate(["/play", this.playerNumber]);
+    this.router.navigate(['/play', this.playerNumber]);
   }
 
   toggleCharacter(char) {
-    if (char == "percival") {
-      this.characters["percival"] = !this.characters["percival"];
+    if (char === 'percival') {
+      this.characters['percival'] = !this.characters['percival'];
     } else {
       if (this.characters[char] || this.minionNumber > 0) {
         this.characters[char] = !this.characters[char];
       }
     }
-
     this.updateNumbers();
   }
 
   updateNumbers() {
     this.minionNumber =
       this.evil[this.playerNumber] -
-      (~~this.characters["mordred"] +
-        ~~this.characters["morgana"] +
-        ~~this.characters["oberon"]);
+      (~~this.characters['mordred'] +
+        ~~this.characters['morgana'] +
+        ~~this.characters['oberon'] + 1);
     this.servantNumber =
       this.playerNumber -
       this.evil[this.playerNumber] -
-      ~~this.characters["percival"] -
+      ~~this.characters['percival'] -
       1;
   }
 }
