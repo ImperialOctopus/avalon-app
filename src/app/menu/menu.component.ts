de1bb72ade5583c314a2259a76d04d64a3e00a55
Zoey Child <imperialoctopus@gmail.com>
2019-03-06 01:58:52 +0000

    Move character booleans into object

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,33 +1,66 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { MatListModule } from '@angular/material/list';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { MatListModule } from "@angular/material/list";
 
 @Component({
-  selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  selector: "app-menu",
+  templateUrl: "./menu.component.html",
+  styleUrls: ["./menu.component.scss"]
 })
 export class MenuComponent implements OnInit {
-  playerNumber: number;
-  oberonEnabled: boolean;
-  morgannaEnabled: boolean;
+  evil: Object = {
+    5: 2,
+    6: 2,
+    7: 3,
+    8: 3,
+    9: 3,
+    10: 4
+  };
+  characters: Object = {
+    percival: false,
+    mordred: false,
+    oberon: false,
+    morgana: false
+  };
 
-  constructor(private router: Router) { }
+  playerNumber: number = 5;
+  servantNumber: number;
+  minionNumber: number;
+
+  constructor(private router: Router) {}
 
   ngOnInit() {
-    this.playerNumber = 5;
-    this.oberonEnabled = false;
-    this.morgannaEnabled = false;
+    this.updateNumbers();
   }
 
   play() {
-    this.router.navigate(['/play', this.playerNumber]);
+    this.router.navigate(["/play", this.playerNumber]);
   }
 
-  oberon() {
-    this.oberonEnabled = !this.oberonEnabled;
+  toggleCharacter(char) {
+    if (char == "percival") {
+      this.characters["percival"] = !this.characters["percival"];
+    }
+    if (char == "mordred") {
+      if (this.characters["mordred"] || this.minionNumber > 0) {
+        this.characters["mordred"] = !this.characters["mordred"];
+      } else {
+      }
+    }
+
+    this.updateNumbers();
   }
-  morganna() {
-    this.morgannaEnabled = !this.morgannaEnabled;
+
+  updateNumbers() {
+    this.minionNumber =
+      this.evil[this.playerNumber] -
+      (~~this.characters["mordred"] +
+        ~~this.characters["morgana"] +
+        ~~this.characters["oberon"]);
+    this.servantNumber =
+      this.playerNumber -
+      this.evil[this.playerNumber] -
+      ~~this.characters["percival"] -
+      1;
   }
 }
