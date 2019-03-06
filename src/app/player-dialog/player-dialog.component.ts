f796226d2e8f9aabfd0f9b4950288424584404dc
Zoey Child <imperialoctopus@gmail.com>
2019-03-06 14:27:29 +0000

    Place close function on number

--- src/app/player-dialog/player-dialog.component.ts
+++ src/app/player-dialog/player-dialog.component.ts
@@ -10,18 +10,31 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 export class PlayerDialogComponent implements OnInit {
 
   description: string;
+  playerNum: number;
 
   constructor(
     private dialogRef: MatDialogRef<PlayerDialogComponent>,
     @Inject(MAT_DIALOG_DATA) data) {
 
     this.description = data.description;
+    this.playerNum = data.playerNum;
   }
 
   ngOnInit() { }
 
+  down() {
+    if (this.playerNum > 5) {
+      this.playerNum -= 1;
+    }
+  }
+  up() {
+    if (this.playerNum < 10) {
+      this.playerNum += 1;
+    }
+  }
+
   save() {
-    this.dialogRef.close('data');
+    this.dialogRef.close(this.playerNum);
   }
 
   close() {
