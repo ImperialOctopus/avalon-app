952ba126dfa6f2bcd703bed63dd3c7b4cb195aac
Zoey Child <imperialoctopus@gmail.com>
2019-03-06 23:30:12 +0000

    Add settings menu opening function

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { MatListModule } from '@angular/material/list';
-import { MatDialog, MatDialogConfig } from '@angular/material';
-import { PlayerDialogComponent } from '../player-dialog/player-dialog.component';
-import { SettingsService } from '../settings.service';
-
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { MatListModule } from "@angular/material/list";
+import { MatDialog, MatDialogConfig } from "@angular/material";
+import { PlayerDialogComponent } from "../player-dialog/player-dialog.component";
+import { SettingsService } from "../settings.service";
+import { SettingsDialogComponent } from "../settings-dialog/settings-dialog.component";
 
 @Component({
-  selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  selector: "app-menu",
+  templateUrl: "./menu.component.html",
+  styleUrls: ["./menu.component.scss"]
 })
 export class MenuComponent implements OnInit {
   evil: object = {
@@ -31,7 +31,11 @@ export class MenuComponent implements OnInit {
   servantNumber: number;
   minionNumber: number;
 
-  constructor(private router: Router, private dialog: MatDialog, private settingsService: SettingsService) { }
+  constructor(
+    private router: Router,
+    private dialog: MatDialog,
+    private settingsService: SettingsService
+  ) {}
 
   ngOnInit() {
     this.playerNumber = this.settingsService.playerNumber;
@@ -39,12 +43,12 @@ export class MenuComponent implements OnInit {
   }
 
   play() {
-    this.router.navigate(['/play']);
+    this.router.navigate(["/play"]);
   }
 
   toggleCharacter(char) {
-    if (char === 'percival') {
-      this.characters['percival'] = !this.characters['percival'];
+    if (char === "percival") {
+      this.characters["percival"] = !this.characters["percival"];
     } else {
       if (this.characters[char] || this.minionNumber > 0) {
         this.characters[char] = !this.characters[char];
@@ -56,26 +60,29 @@ export class MenuComponent implements OnInit {
   updateServants() {
     this.minionNumber =
       this.evil[this.playerNumber] -
-      (~~this.characters['mordred'] +
-        ~~this.characters['morgana'] +
-        ~~this.characters['oberon'] + 1);
+      (~~this.characters["mordred"] +
+        ~~this.characters["morgana"] +
+        ~~this.characters["oberon"] +
+        1);
     this.servantNumber =
       this.playerNumber -
       this.evil[this.playerNumber] -
-      ~~this.characters['percival'] -
+      ~~this.characters["percival"] -
       1;
   }
 
   openPlayerDialog() {
-
     const dialogConfig = new MatDialogConfig();
-
     dialogConfig.autoFocus = false;
-
     const dialogRef = this.dialog.open(PlayerDialogComponent, dialogConfig);
     dialogRef.afterClosed().subscribe(result => {
       this.playerNumber = this.settingsService.playerNumber;
       this.updateServants();
     });
+  }
+  openSettingsDialog() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.autoFocus = false;
+    const dialogRef = this.dialog.open(SettingsDialogComponent, dialogConfig);
   }
 }
