7280e1558be11afe9cb909a6dce100ce5b535c59
Zoey Child <imperialoctopus@gmail.com>
2019-03-06 22:37:24 +0000

    Copy player dialog into settings

--- src/app/settings-dialog/settings-dialog.component.ts
+++ src/app/settings-dialog/settings-dialog.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from "@angular/core";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material";
+import { SettingsService } from "../settings.service";
 
 @Component({
-  selector: 'app-settings-dialog',
-  templateUrl: './settings-dialog.component.html',
-  styleUrls: ['./settings-dialog.component.scss']
+  selector: "app-player-dialog",
+  templateUrl: "./player-dialog.component.html",
+  styleUrls: ["./player-dialog.component.scss"]
 })
-export class SettingsDialogComponent implements OnInit {
+export class PlayerDialogComponent implements OnInit {
+  playerNum: number;
 
-  constructor() { }
+  constructor(
+    private dialogRef: MatDialogRef<PlayerDialogComponent>,
+    private settingsService: SettingsService
+  ) {}
 
   ngOnInit() {
+    this.playerNum = this.settingsService.playerNumber;
   }
 
+  down() {
+    if (this.playerNum > 5) {
+      this.playerNum -= 1;
+    }
+    this.settingsService.setPlayerNumber(this.playerNum);
+  }
+  up() {
+    if (this.playerNum < 10) {
+      this.playerNum += 1;
+    }
+    this.settingsService.setPlayerNumber(this.playerNum);
+  }
+
+  close() {
+    this.dialogRef.close();
+  }
 }
