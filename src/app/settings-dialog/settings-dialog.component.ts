e51887b5b274fb2811310647e7f92ebe1eec298a
Zoey Child <imperialoctopus@gmail.com>
2019-03-07 01:47:43 +0000

    Run tslint

--- src/app/settings-dialog/settings-dialog.component.ts
+++ src/app/settings-dialog/settings-dialog.component.ts
@@ -1,19 +1,19 @@
-import { Component, OnInit, Inject } from "@angular/core";
-import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material";
-import { SettingsService } from "../settings.service";
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { SettingsService } from '../settings.service';
 
 @Component({
-  selector: "app-settings-dialog",
-  templateUrl: "./settings-dialog.component.html",
-  styleUrls: ["./settings-dialog.component.scss"]
+  selector: 'app-settings-dialog',
+  templateUrl: './settings-dialog.component.html',
+  styleUrls: ['./settings-dialog.component.scss']
 })
 export class SettingsDialogComponent implements OnInit {
   constructor(
     private dialogRef: MatDialogRef<SettingsDialogComponent>,
     private settingsService: SettingsService
-  ) {}
+  ) { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
   close() {
     this.dialogRef.close();
