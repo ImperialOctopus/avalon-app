d6920a03acb1394f433d0c0c05707b0a56e2f402
Zoey Child <imperialoctopus@gmail.com>
2019-03-07 17:54:31 +0000

    Import angular forms

--- src/app/settings-dialog/settings-dialog.component.ts
+++ src/app/settings-dialog/settings-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+
 import { SettingsService } from '../settings.service';
 
 @Component({
