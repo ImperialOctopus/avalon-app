f82a210ca0afca2f2b52c7e7c0389a8b2e10c7c1
Zoey Child <imperialoctopus@gmail.com>
2019-03-12 17:40:26 +0000

    Add silence to script

--- src/app/announcer.ts
+++ src/app/announcer.ts
@@ -14,37 +14,54 @@ export class Announcer {
 
     array.push(new SoundElement('everyone', 'Everyone', '/assets/characters/merlin.jpg'));
     array.push(new SoundElement('close-your-eyes', '', ''));
+    array.push(new SoundElement('silence-500', '', ''));
     // (and extend your hand into a fist in front of you)
     //   [and shut up]
     array.push(new SoundElement('minions-of-mordr', '', ''));
     // {(except Oberon)}
     array.push(new SoundElement('open-your-eyes', '', ''));
+    array.push(new SoundElement('silence-1000', '', ''));
+    array.push(new SoundElement('silence-1000', '', ''));
+    array.push(new SoundElement('silence-1000', '', ''));
     // (and look around so that you know the other agents of Evil)
     array.push(new SoundElement('minions-of-mordr', '', ''));
     array.push(new SoundElement('close-your-eyes', '', ''));
+    array.push(new SoundElement('silence-500', '', ''));
     // -(Eyes closed)
     array.push(new SoundElement('minions-of-mordr', '', ''));
     // {(not Mordred himself)}
     array.push(new SoundElement('stick-up-your-th', '', ''));
+    array.push(new SoundElement('silence-500', '', ''));
     // (so that Merlin will know of you)
     array.push(new SoundElement('merlin', '', ''));
     array.push(new SoundElement('open-your-eyes', '', ''));
+    array.push(new SoundElement('silence-1000', '', ''));
+    array.push(new SoundElement('silence-1000', '', ''));
+    array.push(new SoundElement('silence-1000', '', ''));
     // (and see the agents of evil)
     array.push(new SoundElement('minions-of-mordr', '', ''));
     array.push(new SoundElement('put-your-thumbs', '', ''));
+    array.push(new SoundElement('silence-500', '', ''));
     array.push(new SoundElement('merlin', '', ''));
     array.push(new SoundElement('close-your-eyes', '', ''));
+    array.push(new SoundElement('silence-500', '', ''));
     // -(Thumbs down, eyes closed)
     array.push(new SoundElement('merlin-and-morga', '', ''));
     array.push(new SoundElement('stick-up-your-th', '', ''));
+    array.push(new SoundElement('silence-500', '', ''));
     // (so that Percival may know of you)
     array.push(new SoundElement('percival', '', ''));
     array.push(new SoundElement('open-your-eyes', '', ''));
+    array.push(new SoundElement('silence-1000', '', ''));
+    array.push(new SoundElement('silence-1000', '', ''));
+    array.push(new SoundElement('silence-1000', '', ''));
     // (so you may know Merlin and Morgana)
     array.push(new SoundElement('merlin-and-morga', '', ''));
     array.push(new SoundElement('put-your-thumbs', '', ''));
+    array.push(new SoundElement('silence-500', '', ''));
     array.push(new SoundElement('percival', '', ''));
     array.push(new SoundElement('close-your-eyes', '', ''));
+    array.push(new SoundElement('silence-500', '', ''));
     // -(Thumbs down, eyes closed)
     array.push(new SoundElement('everyone', '', ''));
     array.push(new SoundElement('open-your-eyes', '', ''));
