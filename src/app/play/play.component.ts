2a6b31ae8af2b866149db5572d5eb918a6d8e9b3
Zoey Child <imperialoctopus@gmail.com>
2019-03-12 00:32:28 +0000

    Add json data to howler instance

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -37,10 +37,97 @@ export class PlayComponent implements OnInit {
     this._cardText = 'Text sample';
 
     const sound = new Howl({
-      src: ['/assets/audio/onion-capers.mp3']
+      src: [
+        '/assets/audio/en-US_f.ogg',
+        '/assets/audio/en-US_f.m4a',
+        '/assets/audio/en-US_f.mp3',
+        '/assets/audio/en-US_f.ac3'
+      ],
+      sprite: {
+        'and-extend-your': [
+          0,
+          2976.0090702947846
+        ],
+        'and-look-around': [
+          4000,
+          3408.0045351473923
+        ],
+        'and-see-the-agen': [
+          9000,
+          1800.0000000000007
+        ],
+        'and-shut-up': [
+          12000,
+          960.0000000000009
+        ],
+        'close-your-eyes': [
+          14000,
+          1320.0000000000002
+        ],
+        everyone: [
+          17000,
+          912.01814058957
+        ],
+        'except-mordred': [
+          19000,
+          1272.0181405895694
+        ],
+        'except-oberon': [
+          22000,
+          1296.009070294783
+        ],
+        'eyes-closed': [
+          25000,
+          1199.9999999999993
+        ],
+        'merlin-and-morga': [
+          28000,
+          1440.0000000000014
+        ],
+        merlin: [
+          31000,
+          768.0045351473907
+        ],
+        'minions-of-mordr': [
+          33000,
+          1392.0181405895705
+        ],
+        'open-your-eyes': [
+          36000,
+          1272.018140589566
+        ],
+        percival: [
+          39000,
+          864.0136054421746
+        ],
+        'put-your-thumbs': [
+          41000,
+          1320.0000000000002
+        ],
+        'so-that-merlin-w': [
+          44000,
+          1992.0181405895719
+        ],
+        'so-that-percival': [
+          47000,
+          2064.0136054421773
+        ],
+        'so-you-may-know': [
+          51000,
+          2232.018140589567
+        ],
+        'stick-up-your-th': [
+          55000,
+          1416.0090702947841
+        ],
+        'thumbs-down': [
+          58000,
+          1056.0090702947846
+        ]
+      }
     });
 
-    sound.play();
+    sound.play('merlin');
   }
 
   stopButton() {
