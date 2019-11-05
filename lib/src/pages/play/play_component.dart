3eafc713cdc46eb2ca490142c80956c88313a6f9
Zoey Child <imperialoctopus@gmail.com>
2019-11-05 12:06:29 +0000

    Add play component

---
+++ lib/src/pages/play/play_component.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+
+import 'package:angular/angular.dart';
+import 'package:angular_components/angular_components.dart';
+
+@Component(
+  selector: 'todo-list',
+  styleUrls: ['todo_list_component.css'],
+  templateUrl: 'todo_list_component.html',
+  directives: [
+    MaterialCheckboxComponent,
+    MaterialFabComponent,
+    MaterialIconComponent,
+    materialInputDirectives,
+    NgFor,
+    NgIf,
+  ],
+  providers: [],
+)
+class MenuComponent implements OnInit {
+  MenuComponent();
+
+  @override
+  Future<void> ngOnInit() async {}
+}
