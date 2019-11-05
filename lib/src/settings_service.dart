ef0dad5380280bbef4b6f5a1bfad3094fd19c001
Zoey Child <imperialoctopus@gmail.com>
2019-11-05 16:03:13 +0000

    Add enum to settings

--- lib/src/settings_service.dart
+++ lib/src/settings_service.dart
@@ -1,26 +1,18 @@
 enum Setting { playerNumber }
 
 class SettingsService {
-  Map<Setting, dynamic> _settings = {
+  static const defaultSettings = {
     Setting.playerNumber: 5,
   };
+  Map<Setting, dynamic> _settings;
 
   SettingsService();
 
   void initialise() async {
-    await Future.wait([
-      (Future.value(5))
-          .then((int value) => _settings[Setting.playerNumber] = value)
-          .catchError((Error error) => _settings[Setting.playerNumber] = 5),
-    ]);
-  }
+    var box = await Hive.openBox('myBox');
+    
+    defaultSettings.forEach((Setting setting, dynamic value) {
 
-  dynamic getSetting(Setting setting) {
-    return _settings[setting];
-  }
-
-  void setSetting(Setting setting, dynamic value) {
-    _settings[setting] = value;
-    // TODO Save value
+    });
   }
 }
