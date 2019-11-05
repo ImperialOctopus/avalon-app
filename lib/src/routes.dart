a59ff2c7462a173d7dc569f8c3157fea7b86bf86
Zoey Child <imperialoctopus@gmail.com>
2019-11-05 15:28:41 +0000

    Update routes with play page

--- lib/src/routes.dart
+++ lib/src/routes.dart
@@ -1,14 +1,20 @@
 import 'package:angular_router/angular_router.dart';
 
-import 'menu_component.template.dart' as menu_template;
+import 'pages/menu/menu_component.template.dart' as menu_template;
+import 'pages/play/play_component_template.dart' as play_template;
 
 class Routes {
-  static final heroes = RouteDefinition(
+  static final menu_page = RouteDefinition(
     routePath: RoutePath(path: 'menu'),
     component: menu_template.MenuComponentNgFactory,
   );
+  static final play_page = RouteDefinition(
+    routePath: RoutePath(path: 'play'),
+    component: play_template.PlayComponentNgFactory,
+  );
 
   static final all = <RouteDefinition>[
-    heroes,
+    menu_page,
+    play_page,
   ];
 }
