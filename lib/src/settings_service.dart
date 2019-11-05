43c822df7c276e90e155fa4ac405cae48bb9b03f
Zoey Child <imperialoctopus@gmail.com>
2019-11-05 15:25:01 +0000

    Add settings service

--- lib/src/settings_service.dart
+++ lib/src/settings_service.dart
@@ -1,3 +1,26 @@
+enum Setting { playerNumber }
+
 class SettingsService {
-  int get playerNumber => 5;
+  Map<Setting, dynamic> _settings = {
+    Setting.playerNumber: 5,
+  };
+
+  SettingsService();
+
+  void initialise() async {
+    await Future.wait([
+      (Future.value(5))
+          .then((int value) => _settings[Setting.playerNumber] = value)
+          .catchError((Error error) => _settings[Setting.playerNumber] = 5),
+    ]);
+  }
+
+  dynamic getSetting(Setting setting) {
+    return _settings[setting];
+  }
+
+  void setSetting(Setting setting, dynamic value) {
+    _settings[setting] = value;
+    // TODO Save value
+  }
 }
