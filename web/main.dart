986e6b53e7cbd5f3fe5dc29b83e1c3fb6f66cf81
Zoey Child <imperialoctopus@gmail.com>
2019-11-05 10:28:02 +0000

    Inject routing into main app

--- web/main.dart
+++ web/main.dart
@@ -1,8 +1,16 @@
 import 'package:angular/angular.dart';
+import 'package:angular_router/angular_router.dart';
 import 'package:avalon_app/app_component.template.dart' as ng;
 import 'package:pwa/client.dart' as pwa;
 
+import 'main.template.dart' as self;
+
+@GenerateInjector(
+  routerProvidersHash, // TODO Change to routerProviders for production
+)
+final InjectorFactory injector = self.injector$Injector;
+
 void main() {
   pwa.Client();
-  runApp(ng.AppComponentNgFactory);
+  runApp(ng.AppComponentNgFactory, createInjector: injector);
 }
